c671022c6d421198980b79a8d6a3320b6693847d
Thabb <49516328+Thabb@users.noreply.github.com>
2021-10-27 23:19:13 +0200

    ADD: Right-click functionality to PlayerInputManager
    Right-clicking now sends the currently controlled unit to the mouse position.

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -34,5 +34,37 @@ public class PlayerInputController : MonoBehaviour
                 }
             }
         }
+        
+        // right mouse click
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, 200))
+            {
+                // transcribe the x and z value of the hit in a new Vector
+                Vector3 destination = hit.point;
+                destination.y = 1;
+                
+                Debug.Log(destination);
+
+                // case: clicked on open ground
+                if (hit.transform.gameObject.CompareTag("Ground"))
+                {
+                    controlledUnit.GetComponent<Unit>().destPoint = destination;
+                }
+                
+                // TODO: case: clicked on friendly unit or structure
+                
+
+                // TODO: case: clicked on enemy unit or structure
+
+
+                // TODO: case: clicked on enemy structure
+                
+                
+            }
+        }
     }
 }
