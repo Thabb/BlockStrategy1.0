bfccbc29b9691d784c60a97bb5ee75887367964f
Thabb <49516328+Thabb@users.noreply.github.com>
2021-11-10 22:34:55 +0100

    ADD: MenuController and MenuParent from previous project
    Those two work perfectly fine and are completly documentated, so why not use them?

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Menu
+{
+    /// <summary>
+    /// This class is responsible for managing the opening and closing of different menus.
+    /// </summary>
+    /// <remarks>
+    /// Menus get stacked on top of each other on their own as long as you use the Open and Close functions!
+    /// Also this class manages the use of the Escape Key in relevance to menus. But thats probably not what you're searching for. If you wanna change that, take a look at the Update function. Cheers.   
+    /// </remarks>
+    public class MenuController : MonoBehaviour
+    {
+        private GameObject _mainMenu;
+
+        public List<GameObject> menuList = new List<GameObject>();
+
+        /// <summary>
+        /// In this function the main menu or pause menu is referenced. This is the menu that you want to be opened and closed by the Esc key.
+        /// </summary>
+        private void Start()
+        {
+            _mainMenu = transform.GetChild(0).gameObject;
+        }
+
+        /// <summary>
+        /// The Update function manages the opening and closing of the main menu by pressing Esc.
+        /// </summary>
+        /// <remarks>
+        /// This is NOT relevant for someone who wants to implement a new menu. This only used to make the Escape Key work for menus as it should. If you're searching for functionality for new menus take a look at the Open and Close functions below.
+        /// </remarks>
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (menuList.Any())
+                {
+                    Close(menuList.ElementAt(menuList.Count - 1));
+                }
+                else
+                {
+                    Open(_mainMenu);
+                }
+            }
+        }
+
+        
+        // Down here are the two main functions that may interest someone who wants to implement a new menu.
+        
+        /// <summary>
+        /// The main function to open a new menu and append it to the open menus list.
+        /// </summary>
+        /// <remarks>
+        /// Use this function if you wanna open up a new menu. As in you wanna have a button that opens up the next menu or something.
+        /// </remarks>
+        /// <param name="menu">The gameobject of the menu you want to open.</param>
+        public void Open(GameObject menu)
+        {
+            if (menuList.Any())
+            {
+                menuList.ElementAt(menuList.Count - 1).SetActive(false);
+            }
+            
+            menuList.Add(menu);
+            menu.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        /// <summary>
+        /// The main function for closing an already open menu.
+        /// </summary>
+        /// <remarks>
+        /// Use this function if you wanna close an already open menu. As in the button that closes a certain menu.
+        /// </remarks>
+        /// <param name="menu">The gameobject if the menu you want to close.</param>
+        public void Close(GameObject menu)
+        {
+            menu.SetActive(false);
+            menuList.Remove(menu);
+
+            if (menuList.Any())
+            {
+                menuList.ElementAt(menuList.Count - 1).SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MenuParent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    /// <summary>
+    /// The parent class for any kind of menu.
+    /// </summary>
+    /// <remarks>
+    /// This class is important for the functionality of opening up new menus. Use this class as superclass to every new Menu script that you want to implement.
+    /// </remarks>
+    public class MenuParent : MonoBehaviour
+    {
+        internal MenuController MenuController;
+
+        /// <summary>
+        /// Initializes the menucontroller for all the subclasses to use.
+        /// </summary>
+        private void Start()
+        {
+            MenuController = FindObjectOfType<MenuController>();
+        }
+    }
+}
