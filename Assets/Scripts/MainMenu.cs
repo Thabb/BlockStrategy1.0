78c9bb7cd7f5125bfbd4dec01c61296cdddcbcea
Thabb <49516328+Thabb@users.noreply.github.com>
2021-11-10 22:40:52 +0100

    ADD: MainMenu Script
    Has two functions, for the resume and the exit button respectivly.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Menu
+{
+    /// <summary>
+    /// The Main menu that is opened by pressing Esc.
+    /// </summary>
+    /// <remarks>
+    /// The main menu is responsible for the exiting of the game as well as different kind of option menus.
+    /// </remarks>
+    public class MainMenu : MenuParent
+    {
+        /// <summary>
+        /// Closes the menu and resumes the game.
+        /// </summary>
+        public void ResumeButton()
+        {
+            MenuController.Close(gameObject);
+        }
+
+        public void ExitButton()
+        {
+            Application.Quit();
+        }
+    }
+}
