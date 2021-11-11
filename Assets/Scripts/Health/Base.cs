0b7503bd860a8c05cf2c08d2e03778bc75fd0831
Thabb <49516328+Thabb@users.noreply.github.com>
2021-11-11 23:37:35 +0100

    ADD: Documentation for Base class

--- Assets/Scripts/Health/Base.cs
+++ Assets/Scripts/Health/Base.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace Health
 {
+    /// <summary>
+    /// The base is responsible for generating the units the player wants to build and manages the amount of gold a player has in their possession.
+    /// </summary>
     public class Base : HealthEntity
     {
         public GameObject soldierPrefab;
@@ -13,6 +16,9 @@ namespace Health
         public float Gold { get; set; } = 0;
         public TMP_Text goldCounter;
 
+        /// <summary>
+        /// Starts the Coroutine for health regeneration and sets the amount of start gold.
+        /// </summary>
         private new void Start()
         {
             base.Start();
@@ -20,6 +26,10 @@ namespace Health
             ChangeGoldAmount(200);
         }
 
+        /// <summary>
+        /// An asynchronous function with the sole responsibility of regenerating the bases health, 10 points every second.
+        /// </summary>
+        /// <returns>The necessary IEnumerator needed for a Coroutine.</returns>
         private IEnumerator HealthRegeneration()
         {
             while (true)
@@ -38,6 +48,10 @@ namespace Health
             }
         }
 
+        /// <summary>
+        /// Generates a soldier unit at an empty space around the base.
+        /// </summary>
+        /// <returns>True if the generation was successful, false if there was a problem, either with the amount of gold or the available space.</returns>
         public bool GenerateSoldier()
         {
             // check if spawn position is free
@@ -56,6 +70,10 @@ namespace Health
             }
         }
 
+        /// <summary>
+        /// Generates an archer unit at an empty space around the base.
+        /// </summary>
+        /// <returns>True if the generation was successful, false if there was a problem, either with the amount of gold or the available space.</returns>
         public bool GenerateArcher()
         {
             // check if spawn position is free
@@ -74,6 +92,10 @@ namespace Health
             }
         }
 
+        /// <summary>
+        /// Generates a lancer unit at an empty space around the base.
+        /// </summary>
+        /// <returns>True if the generation was successful, false if there was a problem, either with the amount of gold or the available space.</returns>
         public bool GenerateLancer()
         {
             // check if spawn position is free
@@ -92,6 +114,10 @@ namespace Health
             }
         }
 
+        /// <summary>
+        /// Checks if there is an empty space around the base to build a new unit at.
+        /// </summary>
+        /// <returns>An empty position where the unit should be build. Return value equals new Vector() if there is no free position.</returns>
         private Vector3 GetUnitGenerationPosition()
         {
             Vector3 checkPosition;
@@ -134,12 +160,19 @@ namespace Health
             GenerateLancer();
         }
 
+        /// <summary>
+        /// Changes the amount of gold the player/ai has. This also automatically changes the UI gold counter if it is the player.
+        /// </summary>
+        /// <param name="gold"></param>
         public void ChangeGoldAmount(float gold)
         {
             Gold += gold;
             if (team == 1) goldCounter.text = "Gold: " + Gold;
         }
 
+        /// <summary>
+        /// Displays a win/loose message, depending on which base was destroyed.
+        /// </summary>
         private void OnDestroy()
         {
             // TODO: If one base was destroyed the game should end and depending on whos base its was the player will see a "You won" or "You lost" message (UI)
